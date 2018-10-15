16f45b96c6a790ae6edd69f8e52f9eec3dc1db97
Phu Luong <phult.contact@gmail.com>
2018-10-15 15:16:08 +0700

    Using proxy

--- controllers/tracer-controller.js
+++ controllers/tracer-controller.js
@@ -1,13 +1,19 @@
 module.exports = TracerController;
 const puppeteer = require('puppeteer');
+const proxyChain = require('proxy-chain');
 
 function TracerController($config, $event, $logger) {
     this.redirection = function (io) {
         var result = [];
         var url = decodeURIComponent(io.inputs["url"]);
         (async () => {
+            const proxy = await proxyChain.anonymizeProxy(getProxy());
             const browser = await puppeteer.launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+                args: [
+                    '--no-sandbox',
+                    '--disable-setuid-sandbox',
+                    '--proxy-server=' + proxy
+                ],
                 headless: true,
                 timeout: 30000,
                 ignoreHTTPSErrors: true
@@ -36,5 +42,12 @@ function TracerController($config, $event, $logger) {
             }
             io.json(result);
         })();
+    }
+    function getProxy() {
+        var username = 'lum-customer-hl_8bc69a16-zone-static';
+        var password = 'ljhv5rpi3kg6';
+        var port = 22225;
+        var session_id = (1000000 * Math.random()) | 0;
+        return 'http://' + username + '-country-us-session-' + session_id + ':' + password + '@zproxy.lum-superproxy.io:' + port;
     }
 }
