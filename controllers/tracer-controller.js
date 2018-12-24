638221bbc082b42d010b021c530f33bb44c2448b
Phu Luong <phult.contact@gmail.com>
2018-12-24 14:32:14 +0700

    using limiter instead of proxy

--- controllers/tracer-controller.js
+++ controllers/tracer-controller.js
@@ -1,4 +1,6 @@
 module.exports = TracerController;
+var RateLimiter = require('limiter').RateLimiter;
+var requestLimiter = new RateLimiter(1, 2000);
 const puppeteer = require('puppeteer');
 const proxyChain = require('proxy-chain');
 const proxyUrl = 'http://zproxy.lum-superproxy.io:22225';
@@ -8,44 +10,46 @@ const password = 'ljhv5rpi3kg6';
 
 function TracerController($config, $event, $logger) {
     this.redirection = function (io) {
-        var result = [];
-        var url = decodeURIComponent(io.inputs["url"]);
-        (async () => {
-            const browser = await puppeteer.launch({
-                args: [
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--proxy-server=' + proxyUrl
-                ],
-                headless: true,
-                timeout: 30000,
-                ignoreHTTPSErrors: true
+        ((io) => {
+            requestLimiter.removeTokens(1, async function () {
+                var result = [];
+                var url = decodeURIComponent(io.inputs["url"]);
+                const browser = await puppeteer.launch({
+                    args: [
+                        '--no-sandbox',
+                        '--disable-setuid-sandbox',
+                        // '--proxy-server=' + proxyUrl
+                    ],
+                    headless: true,
+                    timeout: 30000,
+                    ignoreHTTPSErrors: true
+                });
+                const page = await browser.newPage();
+                await page.setUserAgent(userAgent);
+                // await page.authenticate({ username, password });
+                try {
+                    await page.on('response', response => {
+                        const url = response.url();
+                        const status = response.status();
+                        const contentType = response.headers()['content-type'];
+                        result.push({
+                            "url": url,
+                            "status": status,
+                            "contentType": contentType,
+                        });
+                        if (status == 404 || (status == 200 && contentType != null && contentType.indexOf('text/html') >= 0)) {
+                            io.json(result);
+                        }
+                    })
+                    await page.goto(url, { waitUntil: 'networkidle0' });
+                    await page.close();
+                    await browser.close();
+                } catch (error) {
+                    await page.close();
+                    await browser.close();
+                }
+                io.json(result);
             });
-            const page = await browser.newPage();
-            await page.setUserAgent(userAgent);
-            await page.authenticate({ username, password });
-            try {
-                await page.on('response', response => {
-                    const url = response.url();
-                    const status = response.status();
-                    const contentType = response.headers()['content-type'];
-                    result.push({
-                        "url": url,
-                        "status": status,
-                        "contentType": contentType,
-                    });
-                    if (status == 404 || (status == 200 && contentType != null && contentType.indexOf('text/html') >= 0)) {
-                        io.json(result);
-                    }
-                })
-                await page.goto(url, { waitUntil: 'networkidle0' });
-                await page.close();
-                await browser.close();
-            } catch (error) {
-                await page.close();
-                await browser.close();
-            }
-            io.json(result);
-        })();
+        })(io);
     }
 }
