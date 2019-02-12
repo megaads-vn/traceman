7bcd5742d0517d36094abcc7a9db0d61fc1af316
Phu Luong <phult.contact@gmail.com>
2019-02-12 14:59:49 +0700

    using cache

--- controllers/tracer-controller.js
+++ controllers/tracer-controller.js
@@ -1,6 +1,8 @@
 module.exports = TracerController;
 var RateLimiter = require('limiter').RateLimiter;
 var requestLimiter = new RateLimiter(1, 1000);
+const Cache = require("cache");
+var cache = new Cache(1800 * 1000);
 const puppeteer = require('puppeteer');
 const proxyChain = require('proxy-chain');
 const proxyUrl = 'http://zproxy.lum-superproxy.io:22225';
@@ -11,45 +13,52 @@ const password = 'ljhv5rpi3kg6';
 function TracerController($config, $event, $logger) {
     this.redirection = function (io) {
         ((io) => {
-            requestLimiter.removeTokens(1, async function () {
-                var result = [];
-                var url = decodeURIComponent(io.inputs["url"]);
-                const browser = await puppeteer.launch({
-                    args: [
-                        '--no-sandbox',
-                        '--disable-setuid-sandbox',
-                        // '--proxy-server=' + proxyUrl
-                    ],
-                    headless: true,
-                    timeout: 30000,
-                    ignoreHTTPSErrors: true
+            var cachedResult = cache.get(io.inputs["url"]);
+            if (cachedResult != null) {
+                io.json(cachedResult);
+            } else {
+                requestLimiter.removeTokens(1, async function () {
+                    var result = [];
+                    var url = decodeURIComponent(io.inputs["url"]);
+                    const browser = await puppeteer.launch({
+                        args: [
+                            '--no-sandbox',
+                            '--disable-setuid-sandbox',
+                            // '--proxy-server=' + proxyUrl
+                        ],
+                        headless: true,
+                        timeout: 30000,
+                        ignoreHTTPSErrors: true    
+                    });
+                    const page = await browser.newPage();
+                    await page.setUserAgent(userAgent);
+                    // await page.authenticate({ username, password });
+                    try {
+                        await page.on('response', response => {
+                            const url = response.url();
+                            const status = response.status();
+                            const contentType = response.headers()['content-type'];
+                            console.log("response", response)
+                            result.push({
+                                "url": url,
+                                "status": status,
+                                "contentType": contentType,
+                            });
+                            // if (status == 404) {
+                            //     io.json(result);
+                            // }
+                        })
+                        await page.goto(url, { waitUntil: 'networkidle0' });
+                        await page.close();
+                        await browser.close();
+                    } catch (error) {
+                        await page.close();
+                        await browser.close();
+                    }
+                    cache.put(io.inputs["url"], result);
+                    io.json(result);
                 });
-                const page = await browser.newPage();
-                await page.setUserAgent(userAgent);
-                // await page.authenticate({ username, password });
-                try {
-                    await page.on('response', response => {
-                        const url = response.url();
-                        const status = response.status();
-                        const contentType = response.headers()['content-type'];
-                        result.push({
-                            "url": url,
-                            "status": status,
-                            "contentType": contentType,
-                        });
-                        if (status == 404 || (status == 200 && contentType != null && contentType.indexOf('text/html') >= 0)) {
-                            io.json(result);
-                        }
-                    })
-                    await page.goto(url, { waitUntil: 'networkidle0' });
-                    await page.close();
-                    await browser.close();
-                } catch (error) {
-                    await page.close();
-                    await browser.close();
-                }
-                io.json(result);
-            });
+            }            
         })(io);
     }
 }
