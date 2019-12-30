89ecb5c8f33a9ca2bccda898c2c68095eba4ed9e
Phu Luong <phult.contact@gmail.com>
2019-12-30 14:30:04 +0700

    Feature: add config example

---
+++ config.example/proxies.js
@@ -0,0 +1,42 @@
+module.exports = {
+    default: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-us",
+        password: "ljhv5rpi3kg6"
+    },
+    us: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-us",
+        password: "ljhv5rpi3kg6"
+    },
+    uk: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-gb",
+        password: "ljhv5rpi3kg6"
+    },
+    can: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-ca",
+        password: "ljhv5rpi3kg6"
+    },
+    es: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-es",
+        password: "ljhv5rpi3kg6"
+    },
+    de: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-de",
+        password: "ljhv5rpi3kg6"
+    },
+    fr: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-fr",
+        password: "ljhv5rpi3kg6"
+    },
+    cn: {
+        url: "http://zproxy.lum-superproxy.io:22225",
+        username: "lum-customer-hl_8bc69a16-zone-static-country-sg",
+        password: "ljhv5rpi3kg6"
+    }
+};
