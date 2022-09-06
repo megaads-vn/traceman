5b2ee40c991496be0ca2843de3c91dee2a57316e
Phu Luong <phult.contact@gmail.com>
2022-09-06 16:40:51 +0700

    feature: clusters

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 global.__dir = __dirname;
-require(__dir + "/core/app/start").start();
-process.on('unhandledRejection', error => {
-    // Do not log error when suddenly close browser!
-});
+const totalCPUs = require('os').cpus().length;
+const cluster = require('cluster');
+if (cluster.isMaster) {
+    masterProcess();
+} else {
+    childProcess();
+}
+function masterProcess() {
+    console.log(`Master ${process.pid} is running`);
+    for (let i = 0; i < totalCPUs; i++) {
+        console.log(`Forking process number ${i}...`);
+        const worker = cluster.fork();
+        worker.send({
+            task: 'boot',
+            data: {}
+        });
+    }
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+        console.log("Let's fork another worker!");
+        cluster.fork();
+    });
+}
+function childProcess() {
+    process.on('message', function (msg) {
+        if (msg.task === 'boot') {
+            require(__dir + "/core/app/start").start();
+        }
+    });
+    process.on('unhandledRejection', error => {
+        // Do not log error when suddenly close browser!
+    });
+}
